5d6ea0cf81169dd44e1d08f6d77fe50c2e001d52
josielson-ferreira <142331831+josielson-ferreira@users.noreply.github.com>
2023-08-19 14:33:26 -0300

    Delete questao9.js

--- questao9.js
+++
@@ -1,7 +0,0 @@
-//Escreva um programa JavaScript para verificar um par de
-// números e retornar true se um dos números for 50 ou se a soma for 50.
-function test50(x,y)
-{
-    return ((x==50 || y==50) || (x+y==50));
-}
-console.log(test50(20,20));
