3d6f48db686ff03f34fa7f312b247f93c4612f39
josielson-ferreira <142331831+josielson-ferreira@users.noreply.github.com>
2023-08-19 12:57:28 -0300

    Update and rename questao11.js to questao2.js

--- questao2.js
+++ questao2.js
@@ -3,7 +3,7 @@ function cToF(celsius)
 {
     var cTemp = celsius;
     var cToFahr = cTemp * 9 / 5 + 32;
-    var message = cTemp+ ' \xB0C is ' + cToFahr + ' \xB0F.';
+    var message = cTemp + ' \xB0C is ' + cToFahr + ' \xB0F.';
        console.log(message);
 }
 cToF(60);
