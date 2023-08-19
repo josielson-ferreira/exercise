daefbc75f4c94c26908b6e789dcc69cd2916eced
josielson-ferreira <142331831+josielson-ferreira@users.noreply.github.com>
2023-08-19 14:31:08 -0300

    Delete questao10.js

--- questao10.js
+++
@@ -1,7 +0,0 @@
-//Escreva um programa JavaScript para verificar se 
-//um determinado inteiro está dentro de 20 de 100 ou 400.
-function test(x)
-{
-    return(Math.abs(100 - x) <= 20 || (Math.abs(400 - x) <= 20));
-}
-console.log(test(10));
