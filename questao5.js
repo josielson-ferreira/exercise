37e74a3677802add818c284d9f7937a09b93533f
josielson-ferreira <142331831+josielson-ferreira@users.noreply.github.com>
2023-08-19 14:32:51 -0300

    Delete questao5.js

--- questao5.js
+++
@@ -1,8 +0,0 @@
-//Escreva um programa JavaScript para criar outra string a partir de uma determinada string com o primeiro caractere da string fornecida adicionado à frente e atrás.
-function front_back(str)
-{
-    first = str.substring(0,1);
-    return first + str + first;
-}
-console.log(front_back('a'));
-console.log(front_back('ab'));
